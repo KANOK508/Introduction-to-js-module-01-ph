ff1c919980d2a79f29ea4055fb56a755f8386b72
unknown <kanokfaishalhaque018@gmail.com>
2026-01-28 23:32:17 +0600

    second commit

--- mixedOp.js
+++ mixedOp.js
@@ -2,7 +2,26 @@ var first = 'Mobarok';
 var second = 'Tobarok';
 
 var fullName = first + second;
-// console.log(fullName);
-// console.log('40'+2);
+console.log(fullName);
+console.log('40'+2);
+// string+ number = string+string
+// number - string = number
+// string -number = number 
+
+// those strings are number .... at first 
+
+
 console.log(40-'20');
 console.log('50'-10);
+// so, with string and number the ( substructin and addition work ---differently...)
+
+var a = isNaN('11');
+console.log(a);
+var b = isNaN(2-10);
+console.log(b);
+var c = isNaN(10);
+console.log(c);
+
+console.log(isNaN("hello"));     // true
+console.log(isNaN(undefined));  // true
+console.log(isNaN(0 / 0));      // true
--- number.js
+++ number.js
@@ -3,6 +3,12 @@ var chocolate = 0.5;
 // var applePrice = parseInt('22.5');
 var applePrice = parseFloat('22.5');
 
+var examMark = parseInt('ami300 parichi');
+// when we use parseInt ---- the data type is set to number 
+let assMark = parseFloat(40);
+console.log(assMark);
+console.log(typeof examMark);
+console.log(examMark);
 // console.log(orangePrice);
 // console.log(applePrice);
 // console.log(orangePrice + applePrice);
@@ -11,6 +17,11 @@ var first = 0.1;
 var second = 0.2;
 var total = first + second;
 console.log(total.toFixed(1))
+//  variableName.toFixed(how many number we want after dot....)
+
 const num = -50 / 0;  //        -Infinity
 
 console.log(num);
+
+let check = parseInt(" 20 hell0");    // the 20 is converted to int value
+console.log( check); // 20
